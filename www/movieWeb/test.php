f5fcaa4cd96e705a7d3ce1ea97defdc9bdf7e5c6
HYE <hyebonny1106@naver.com>
2022-09-16 17:28:07 +0900

    0916.02

---
+++ www/movieWeb/test.php
@@ -0,0 +1,71 @@
+<?php
+  include_once $_SERVER["DOCUMENT_ROOT"]."/www/movieWeb/lib/dbconn.php";
+  // include("../lib/dbconn.php");
+  function page($start, $totalCount, $rowPerPage, $blockSet, $first, $pre, $next, $last) {
+    echo "<a href='?start=0'>$first</a>  ";
+
+    $page = floor ($start / ($rowPerPage * $blockSet));
+    if ($totalCount > $rowPerPage) {
+      if($start+1 > $rowPerPage*$blockSet) {
+        $pre_start = $page * $rowPerPage * $blockSet - $rowPerPage;
+        echo "<a href='?start=$pre_start'>$pre</a>";
+      } else echo "$pre";
+    } else echo "$pre";
+
+    if($totalCount > 0) echo "| ";
+
+    for ($vj=0 ; $vj<$blockSet ; $vj++) {
+      $ln = ($page * $blockSet + $vj) * $rowPerPage;
+      $page_num = $page * $blockSet+$vj + 1;
+      if ($ln < $totalCount) {
+        if($ln != $start) echo "<a href='?start=$ln'><b>$page_num</b></a> | ";
+        else echo "[<b>$page_num</b>] | ";
+      }
+    }
+
+    if($totalCount > (($page+1) * $rowPerPage * $blockSet)){
+      $n_start = ($page+1) * $rowPerPage * $blockSet;
+      echo  "<a href='?start=$n_start'>$next</a>";
+    } else echo  $next;
+
+    $end_start = floor($totalCount/$rowPerPage - 1)*$rowPerPage;
+    echo "  <a href='?start=$end_start'>$last</a>";
+  }
+
+  $start = 0;
+	if(isset($_GET["start"])) {
+		$start = $_GET["start"];
+	}
+
+	$sql = "select count(*) as cnt from Author";
+	$result = $conn1->query($sql);
+  $totalCount = $result->fetch_assoc()['cnt'];
+?>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="EUC-KR">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>리스트 페이징</title>
+</head>
+<body>
+  <?php
+    if ($totalCount == 0) {
+      echo "데이터가 없습니다.";
+    } else {
+      $rowPerPage = 2;
+      $blockSet = 2;
+      $link = "";
+
+      $sql = "select * from Author order by id desc limit ".$start.", ".$rowPerPage;
+      $result = $conn1->query($sql);
+      while($rows = $result->fetch_assoc()) {
+        echo $rows["id"].":".$rows["name"]."<br>";
+      }
+    }
+
+    page($start, $totalCount, $rowPerPage, $blockSet, "처음", "이전", "다음", "마지막");
+  ?>
+</body>
+</html>
---
+++ www/oneandthree/js/index_js.js
@@ -0,0 +1,139 @@
+
+// -----width=690(모바일)경우 메뉴변화-----
+let openMenu = document.querySelector('.menu_btn'),
+    slide = document.querySelector('nav'),
+    closeMenu = document.querySelector('.menuClose'),
+    menulistClick = document.querySelectorAll('.menuberList_li>a');
+    menulistOpen = document.querySelectorAll('.menuberList_liCh');
+
+console.log (menulistClick);
+
+openMenu.addEventListener('click', function(){
+    seeMenu();
+});
+closeMenu.addEventListener('click', function(){
+    hideMenu();
+});
+function seeMenu() {
+    slide.style.display="block";
+    slide.style.position="absolute";
+    slide.style.width="70%";
+}
+function hideMenu() {
+    slide.style.display="none";
+}
+
+for(i=0; i<menulistClick.length; i++) {
+    console.log(menulistClick[i]);
+    menulistClick[i].addEventListener('click', function(){ 
+        if (this.nextSibling.nextSibling.style.display == "none") {
+            console.log('1:', this);
+            console.log(this.nextSibling.nextSibling) ;
+            this.nextSibling.nextSibling.style.display = "block";
+        } else {
+            this.nextSibling.nextSibling.style.display = "none";
+        };
+    });
+}
+
+
+// -----main_Imgbox_ 이미지 슬라이드쇼 ------
+let main_banner_Img= [
+    "./img/top_baner1_PC1920x440.png",
+    "https://i.ibb.co/fXxC4j1/main-PC-1920x440-20220722110659575.jpg",
+    "https://i.ibb.co/wzG57vQ/main-PC-1920x440-20220829043050726.jpg",
+    "./img/top_baner4_PC1920x440.png"
+]   
+
+let cnt = 0;
+function chage_banner_Img() {                       
+    document.querySelector('.main_banner_Img').setAttribute('src', main_banner_Img[cnt]);
+    // document.querySelector('.main_banner_Imgbox>a').setAttribute('href', 이미지 자세히보기 주소);
+    cnt = cnt + 1;        
+    if( cnt == 4) {
+        cnt = 0;
+    };        
+}
+setInterval(chage_banner_Img, 2000);
+
+function currentSlide1() {
+    document.querySelector('.main_banner_Img').setAttribute('src', main_banner_Img[1]);
+}
+
+// -----main_menulistWrap Swiper -----
+let menulistCotroll = document.querySelectorAll('.main_ml_topRight>ul>a>li'),
+        menulistTab_Cl = document.getElementsByClassName('main_ml_bottom_cl')[0],
+        menulistTab_fl = document.getElementsByClassName('main_ml_bottom_fl')[0],
+        menulistTab_pm = document.getElementsByClassName('main_ml_bottom_pm')[0],
+        menulistTab_mo = document.getElementsByClassName('main_ml_bottom_mo')[0];
+
+menulistCotroll[0].addEventListener('click', () => {
+    menulistTab_Cl.style.display = 'block';
+    menulistTab_fl.style.display = 'none';
+    menulistTab_pm.style.display = 'none';
+    menulistTab_mo.style.display = 'none';
+});
+
+menulistCotroll[1].addEventListener('click', () => {
+    menulistTab_fl.style.display = 'block';
+    menulistTab_Cl.style.display = 'none';
+    menulistTab_pm.style.display = 'none';
+    menulistTab_mo.style.display = 'none';
+});
+
+menulistCotroll[2].addEventListener('click', () => {
+    menulistTab_pm.style.display = 'block';
+    menulistTab_Cl.style.display = 'none';
+    menulistTab_fl.style.display = 'none';
+    menulistTab_mo.style.display = 'none';
+
+});
+
+menulistCotroll[3].addEventListener('click', () => {
+    menulistTab_mo.style.display = 'block';
+    menulistTab_Cl.style.display = 'none';
+    menulistTab_fl.style.display = 'none';
+    menulistTab_pm.style.display = 'none';
+});
+
+/*Section 공통> Initialize Swiper */
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 2,
+          spaceBetween: 5
+        },
+        // when window width is >= 480px
+        480: {
+          slidesPerView: 3,
+          spaceBetween: 8
+        },
+        // when window width is >= 640px
+        680: {
+          slidesPerView: 4,
+          spaceBetween: 10
+        },
+      },
+    slidesPerGroup: 4,
+    //loop: true>>반복 
+    //loopFillGroupWithBlank: true, >>공백값
+    // loop: true,
+    // loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
+  });
+// ----wrapTopButton_ 탑버튼 -----
+const topBtn = document.getElementById('topBtn');
+topBtn.onclick = () => {
+    window.scrollTo({left: 0, top: 0, behavior: 'smooth'});
+}
